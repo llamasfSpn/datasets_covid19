72a1790036fe843b629b7ba5634f8f2ba4905f9a
llamasfSpn <fran_javier_llamas@hotmail.com>
2020-04-04 18:57:05 +0200

    [MOD]Refactoring code

--- script.py
+++ script.py
@@ -6,9 +6,9 @@
 ############################
 import csv
 from itertools import izip
-import requests
 import git
 import os
+from time import sleep
 
 #Functions GIT
 def git_clone(url):
@@ -32,24 +32,37 @@ if not os.path.isdir('./datasets/output'):
     os.mkdir('./datasets/output')
 
 
-#Deaths People Spain
+
+#Function Revert CSV
 #Revert row and columns, edit rows
 #Treat dataset
+def revert_csv(name,inputFileName,outputFileName,outputFileEndName):
+    print("Start Revert "+name)
+    a = izip(*csv.reader(open(inputFileName, "rb")))
+    csv.writer(open(outputFileName, "wb")).writerows(a)
+    with open(outputFileName, 'rb') as inFile, open(outputFileEndName, 'wb') as outfile:
+        r = csv.reader(inFile)
+        w = csv.writer(outfile)
+        i=1
+        for row in r:
+                if i==1:
+                    w.writerow(['CCAA','Andalucia','Aragon','Asturias','Baleares','Canarias','Cantabria','Castilla-La Mancha','Castilla y Leon','Cataluna','Ceuta','C. Valenciana','Extremadura','Galicia','Madrid','Melilla','Murcia','Navarra','Pais Vasco','La Rioja'])
+                elif i==2:
+                    w.writerow(['date','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string'])
+                else:
+                    w.writerow(row[:-1])
+                i += 1
+    os.remove(outputFileName)
+    sleep(5)
+    print("End Revert "+name)
+
+#Deaths People Spain
 inputFileName="./datasets/COVID 19/ccaa_covid19_fallecidos.csv"
 outputFileName="./datasets/output/ccaa_covid19_fallecidos_test.csv"
 outputFileEndName="./datasets/output/ccaa_covid19_fallecidos_output.csv"
-a = izip(*csv.reader(open(inputFileName, "rb")))
-csv.writer(open(outputFileName, "wb")).writerows(a)
-with open(outputFileName, 'rb') as inFile, open(outputFileEndName, 'wb') as outfile:
-    r = csv.reader(inFile)
-    w = csv.writer(outfile)
-    i=1
-    for row in r:
-            if i==1:
-                w.writerow(['CCAA','Andalucia','Aragon','Asturias','Baleares','Canarias','Cantabria','Castilla-La Mancha','Castilla y Leon','Cataluna','Ceuta','C. Valenciana','Extremadura','Galicia','Madrid','Melilla','Murcia','Navarra','Pais Vasco','La Rioja'])
-            elif i==2:
-                w.writerow(['date','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string','string'])
-            else:
-                w.writerow(row[:-1])
-            i += 1
-os.remove(outputFileName)
+revert_csv("Deaths CSV",inputFileName,outputFileName,outputFileEndName)
+#Infected People Spain
+inputFileName="./datasets/COVID 19/ccaa_covid19_casos.csv"
+outputFileName="./datasets/output/ccaa_covid19_casos_test.csv"
+outputFileEndName="./datasets/output/ccaa_covid19_casos_output.csv"
+revert_csv("Infected CSV",inputFileName,outputFileName,outputFileEndName)
